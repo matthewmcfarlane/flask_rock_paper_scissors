4e1f6e0fb22de976d2ccb61c3817095fa8418b97
Matthew McFarlane <matthewmcfarlane1@outlook.com>
2021-11-26 20:28:03 +0000

    mvp complete

---
+++ controllers/controller.py
@@ -0,0 +1,34 @@
+from flask import render_template, request
+from app import app
+from models.player import Player
+from models.game import choices, determine_winner
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home', stage='Player 1, make your move!')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
+
+@app.route('/<id>/')
+def player_1_choice(id):
+    choice_p1 = id.lower().strip()
+    if choice_p1 in choices:
+        return render_template('index.html', currentpath=id ,title='In-Game', stage='Player 2, make your move!')
+    else:
+        return page_not_found(404)
+
+@app.route('/<id1>/<id2>/')
+def player_2_choice(id1, id2):
+    choice_p1 = id1.lower().strip()
+    choice_p2 = id2.lower().strip()
+    result = determine_winner(choice_p1, choice_p2)
+    if result == None:
+        return render_template("404.html")
+    else:
+        return render_template('index.html', title='Results!', stage=result, again=' Player 1, make your move!',  reset='/')
+
+@app.route('/rules/')
+def rules():
+    return render_template('rules.html', title='Rules')
---
+++ models/game.py
@@ -0,0 +1,24 @@
+
+choices = ['rock', 'paper', 'scissors']
+
+def determine_winner(player_1, player_2):
+    if player_1 == 'rock' and player_2 == 'rock':
+        return "It's a Draw! - Go again!"
+    elif player_1 == 'paper' and player_2 =='paper':
+        return "It's a Draw! - Go again!"
+    elif player_1 == 'scissors' and player_2 =='scissors':
+        return "It's a Draw! - Go again!"
+    elif player_1 == 'rock' and player_2 =='paper':
+        return "Player 2 Wins! - Go again!"
+    elif player_1 == 'paper' and player_2 =='scissors':
+        return "Player 2 Wins! - Go again!"
+    elif player_1 == 'scissors' and player_2 =='rock':
+        return "Player 2 Wins! - Go again!"
+    elif player_1 == 'paper' and player_2 =='rock':
+        return "Player 1 Wins! - Go again!"
+    elif player_1 == 'scissors' and player_2 =='paper':
+        return "Player 1 Wins! - Go again!"
+    elif player_1 == 'rock' and player_2 =='scissors':
+        return "Player 1 Wins! - Go again!"
+    else:
+        return None
