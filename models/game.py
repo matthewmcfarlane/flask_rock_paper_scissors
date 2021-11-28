f78c8351d7e233b43639ba112a3fe9004496ce20
Matthew McFarlane <matthewmcfarlane1@outlook.com>
2021-11-28 23:09:13 +0000

    minor text output changes

--- models/game.py
+++ models/game.py
@@ -27,22 +27,22 @@ class Game:
 
     def determine_winner_cpu(player_1, cpu):
         if player_1 == 'rock' and cpu == 'rock':
-            return "You both chose rock! - Go again"
+            return "You both chose rock!"
         elif player_1 == 'paper' and cpu =='paper':
-            return "You both chose paper! - Go again!"
+            return "You both chose paper!"
         elif player_1 == 'scissors' and cpu =='scissors':
-            return "You both chose scissors! - Go again!"
+            return "You both chose scissors!"
         elif player_1 == 'rock' and cpu =='paper':
-            return f"You chose {player_1} and the computer chose {cpu}, Computer Wins! - Go again!"
+            return f"You chose {player_1} and the computer chose {cpu} -  Computer Wins!"
         elif player_1 == 'paper' and cpu =='scissors':
-            return f"You chose {player_1} and the computer chose {cpu}, Computer Wins! - Go again!"
+            return f"You chose {player_1} and the computer chose {cpu} -  Computer Wins!"
         elif player_1 == 'scissors' and cpu =='rock':
-            return f"You chose {player_1} and the computer chose {cpu}, Computer Wins! - Go again!"
+            return f"You chose {player_1} and the computer chose {cpu} -  Computer Wins!"
         elif player_1 == 'paper' and cpu =='rock':
-            return f"You chose {player_1} and the computer chose {cpu}, You Win! - Go again!"
+            return f"You chose {player_1} and the computer chose {cpu} -  You Win!"
         elif player_1 == 'scissors' and cpu =='paper':
-            return f"You chose {player_1} and the computer chose {cpu}, You Win! - Go again!"
+            return f"You chose {player_1} and the computer chose {cpu} -  You Win!"
         elif player_1 == 'rock' and cpu =='scissors':
-            return f"You chose {player_1} and the computer chose {cpu}, You Win! - Go again!"
+            return f"You chose {player_1} and the computer chose {cpu} -  You Win!"
         else:
             return None
